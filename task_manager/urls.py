1514b718d1e20a349ccffd4861abc1f436554478
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-10-21 00:07:14 +0300

    fixed logout

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Index.as_view(), name='index' ),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
+    path('logout/', views.LogoutView.as_view(), name='logout'), 
     path('users/', include('users.urls')),
     path('statuses/', include('statuses.urls')),
     path('tasks/', include('tasks.urls')),
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views import View
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
 
 class Index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
+
+
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('index')
+    
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('index')
