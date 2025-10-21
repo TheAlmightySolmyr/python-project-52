1ef7cd440598bd4afd0f08ce0af3d9ab4f100c4a
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-10-22 00:43:33 +0300

    added filtration

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.views.generic import (
     CreateView,
     DeleteView,
@@ -10,6 +10,7 @@ from django.views.generic import (
     UpdateView,
 )
 
+from .filters import TaskFilter
 from .forms import TaskForm
 from .models import Task
 
@@ -18,13 +19,31 @@ class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
-    ordering = ['-created_at']
+    
+    def get_queryset(self):
+        queryset = Task.objects.all().order_by('-created_at')
+        self.filter = TaskFilter(self.request.GET, queryset=queryset, request=self.request)
+        return self.filter.qs
+    
+    def get_context_data(self, **kwargs):
+        context = {}
+        if hasattr(self, 'filter'):
+            context['filter'] = self.filter
+        else:
+            queryset = Task.objects.all().order_by('-created_at')
+            context['filter'] = TaskFilter(self.request.GET, queryset=queryset, request=self.request)
+        context['tasks'] = context['filter'].qs
+        return context
 
 
 class TaskDetailView(LoginRequiredMixin, DetailView):
     model = Task
     template_name = 'tasks/task_detail.html'
     context_object_name = 'task'
+    
+    def get_object(self, queryset=None):
+        task_id = self.kwargs.get('pk')
+        return Task.objects.get(id=task_id)
 
 
 class TaskCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
@@ -35,10 +54,21 @@ class TaskCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        return super().form_valid(form)
+        form.save()
+        return redirect(self.get_success_url())
 
     def get_success_url(self):
         return '/tasks/'
+    
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            return self.form_valid(form)
+        return render(request, self.template_name, {'form': form})
 
 
 class TaskUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
@@ -47,8 +77,28 @@ class TaskUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     template_name = 'tasks/task_form.html'
     success_message = 'Задача успешно изменена'
 
+    def form_valid(self, form):
+        form.save()
+        return redirect(self.get_success_url())
+
     def get_success_url(self):
         return '/tasks/'
+    
+    def get_object(self, queryset=None):
+        task_id = self.kwargs.get('pk')
+        return Task.objects.get(id=task_id)
+    
+    def get(self, request, *args, **kwargs):
+        task = self.get_object()
+        form = self.form_class(instance=task)
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        task = self.get_object()
+        form = self.form_class(request.POST, instance=task)
+        if form.is_valid():
+            return self.form_valid(form)
+        return render(request, self.template_name, {'form': form})
 
 
 class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, DeleteView):
@@ -59,10 +109,27 @@ class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixi
     def get_success_url(self):
         return '/tasks/'
 
+    def delete_task(self, request, *args, **kwargs):
+        task = self.get_object()
+        task.delete()
+        messages.success(request, self.success_message)
+        return redirect(self.get_success_url())
+
+    def post(self, request, *args, **kwargs):
+        return self.delete_task(request, *args, **kwargs)
+
     def test_func(self):
         task = self.get_object()
         return self.request.user == task.author
+    
+    def get_object(self, queryset=None):
+        task_id = self.kwargs.get('pk')
+        return Task.objects.get(id=task_id)
 
     def handle_no_permission(self):
-        messages.error(self.request, 'Задачу может удалить только её автор')
+        messages.error(request, 'Задачу может удалить только её автор')
         return redirect('/tasks/')
+    
+    def get(self, request, *args, **kwargs):
+        task = self.get_object()
+        return render(request, self.template_name, {'object': task})
